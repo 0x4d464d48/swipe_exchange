33d56e536ce80c083cea0069ebcadd4e42068988
Michael Mitchell-Halter <0x4d464d48@gmail.com>
2016-09-24 16:13:46 -0700

    Added email test and 'get buy requests' sample to AJAX requests

--- public/js/samplejax.js
+++ public/js/samplejax.js
@@ -1,6 +1,4 @@
-function testPing() {
-    var data_file = "http://www.tutorialspoint.com/json/data.json";
-    var http_request = new XMLHttpRequest();
+function makeRequest(data) {
     var pingRequest = {
         json: {
             jsonrpc: "2.0",
@@ -13,15 +11,89 @@ function testPing() {
         url: '/jsonrpc',
         type: 'post',
         dataType: 'JSON',
-        success: function(data)  {
+        success: function (data) {
             console.log(data);
         },
-        error: function(error) {
+        error: function (error) {
             console.log(pingRequest);
             console.log(error);
         },
-        data: { json: JSON.stringify(pingRequest.json) }});
+        data: { json: JSON.stringify(data) }
+    });
 }
 
+function testPing() {
+    var pingRequest = {
+        json: {
+            jsonrpc: "2.0",
+            id: "id",
+            method: "ping"
+        }
+    };
+
+    $.ajax({
+        url: '/jsonrpc',
+        type: 'post',
+        dataType: 'JSON',
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (error) {
+            console.log(pingRequest);
+            console.log(error);
+        },
+        data: { json: JSON.stringify(pingRequest.json) }
+    });
+}
+
+// Use to test get users
+var userEmails = [
+    'jeremy.quicklearner@gmail.com',
+    'quicklearner@gmail.com',
+    'jeremy@gmail.com',
+    'jql@gmail.com'
+];
+
+// Use to test requests
+var requestParams = [
+    ['mathcurt@gmail.com', '1474746465'],
+    ['derekchan1994@gmail.com', '1474746467'],
+];
+
 // Test the ping forever
-setInterval(testPing, 1000);
+//setInterval(testPing, 1000);
+
+// Test that you can get all users
+for (var email in userEmails) {
+    var currentEmail = userEmails[email];
+    console.log('Gettting user:' + currentEmail);
+
+    var jsonData = {
+        "jsonrpc": "2.0",
+        "id": "id",
+        "method": "get_user",
+        "params": {
+            "user_email": currentEmail
+        }
+    }
+
+    makeRequest(jsonData);
+}
+
+for (var tuple in requestParams) {
+    var currentTuple = requestParams[tuple];
+    console.log('Gettting request:' + currentTuple);
+
+    var jsonData = {
+        "jsonrpc": "2.0",
+        "id": "id",
+        "method": "create_request",
+        "params": {
+            "listing_timestamp": currentTuple[1],
+            "buyer_email": currentTuple[0]
+        }
+
+    }
+
+    makeRequest(jsonData);
+}
