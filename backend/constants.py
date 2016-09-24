b61a8cdd933d07877e08b09b08d428462098eeba
Curtis Huebner <mathcurt@gmail.com>
2016-09-24 13:24:07 -0700

    implemented populate db method

--- backend/constants.py
+++ backend/constants.py
@@ -3,13 +3,5 @@ MONGOD_IP = "localhost"
 MONGOD_PORT = 27017
 DB_NAME = "db"
 
-
-#FILES_AND_COLLECTIONS
-USER_COLLECTION = "users"
-LISTING_COLLECTION =  "listings"
-REQUEST_COLLECTION = "requests"
-
-USERS = "users.json"
-LISTINGS =  "listings.json"
-REQUESTS = "requests.json"
+COLLECTIONS = ("users","listings","requests")
 
