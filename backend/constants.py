c47e6c2d651c845a2b050ad9580f3e12a4f060f0
Curtis Huebner <mathcurt@gmail.com>
2016-09-24 12:04:50 -0700

    Added constants and database API

--- backend/constants.py
+++ backend/constants.py
@@ -1,4 +1,12 @@
 DB_NAME = "database"
 MONGOD_IP = "localhost"
-MONGOD_PORT = "27017"
+MONGOD_PORT = 27017
 DB_NAME = "db"
+USER_COLLECTION = "users"
+LISTING_COLLECTION =  "listings"
+REQUEST_COLLECTION = "requests"
+
+USERS = "users.json"
+LISTINGS =  "listings.json"
+REQUESTS = "requests.json"
+
