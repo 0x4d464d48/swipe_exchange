2d203024ae4d418e8bb348efce8cbccc47036d8c
Derek Chan <derekchan1994@gmail.com>
2016-09-24 15:22:32 -0700

    WebApp: Menu flow for uploading new item

--- public/js/swipetrade.js
+++ public/js/swipetrade.js
@@ -1,3 +1,14 @@
+var activateTab = function(id) {
+  // remove all is-active classes from tabs
+  $('a.mdl-layout__tab').removeClass('is-active');
+  // activate desired tab
+  $('a[href="' + id + '"]').addClass('is-active');
+  // remove all is-active classes from panels
+  $('.mdl-layout__tab-panel').removeClass('is-active');
+  // activate desired tab panel
+  $("div"+id).addClass('is-active');
+}
+
 $(document).ready(function(){
     console.log("Hello!")
     
@@ -12,6 +23,60 @@ $(document).ready(function(){
     
   $("#swipetrade-filter-input").keyup(function(event) {
     console.log("Input contents: %s", $(this).val());
+  });
+  
+  var tabs = {
+    1: "#post-new",
+    2: "#post-new-upload-img",
+    3: "#post-new-set-price",
+    4: "#post-new-set-description",
+    5: "#post-new-overview"
+  }
+  
+  $("#add").click(function() {
+    console.log("Clicked!");
+    activateTab(tabs[1]);
+  });
+    
+  $(".swipetrade-button-post-new").click(function() {
+    console.log("Woohoo" + $(this).text());
+    activateTab(tabs[2]);
+  });
+  
+  $("#swipetrade-button-upload-img").click(function() {
+    console.log("Woohoo2");
+    activateTab(tabs[3]);
+  });
+  $("#swipetrade-button-upload-img-back").click(function() {
+    console.log("Woohoo2");
+    activateTab(tabs[1]);
+  });
+  
+  $("#swipetrade-button-set-price").click(function() {
+    console.log("Woohoo3");
+    activateTab(tabs[4]);
+  });
+  $("#swipetrade-button-set-price-back").click(function() {
+    console.log("Woohoo3");
+    activateTab(tabs[2]);
+  });
+  
+  $("#swipetrade-button-set-description").click(function() {
+    console.log("Woohoo3");
+    activateTab(tabs[5]);
+  });
+  $("#swipetrade-button-set-description-back").click(function() {
+    console.log("Woohoo3");
+    activateTab(tabs[3]);
+  });
+  
+  $("#swipetrade-button-overview").click(function() {
+    console.log("Done, activating post");
+    activateTab("#post");
+  });
+  $("#swipetrade-button-overview-back").click(function() {
+    console.log("Woohoo3");
+    activateTab(tabs[4]);
   });
     
 });
