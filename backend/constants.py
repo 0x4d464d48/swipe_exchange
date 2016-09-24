2618c88ea968fa9923e3e4e8c6fec97548fa8152
Curtis Huebner <mathcurt@gmail.com>
2016-09-24 12:24:18 -0700

    fixed code

--- backend/constants.py
+++ backend/constants.py
@@ -2,6 +2,9 @@ DB_NAME = "database"
 MONGOD_IP = "localhost"
 MONGOD_PORT = 27017
 DB_NAME = "db"
+
+
+#FILES_AND_COLLECTIONS
 USER_COLLECTION = "users"
 LISTING_COLLECTION =  "listings"
 REQUEST_COLLECTION = "requests"
