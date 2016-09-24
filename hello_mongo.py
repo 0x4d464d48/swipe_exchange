bc12bf7eda17a9a2dab1f4bec1474619d410a64a
Michael Mitchell-Halter <0x4d464d48@gmail.com>
2016-09-23 17:31:39 -0700

    Working client

--- hello_mongo.py
+++ hello_mongo.py
@@ -1,4 +1,14 @@
 from pymongo import MongoClient
-c = MongoClient("mongodb://localhost:27017")
-c['test-database']
+client = MongoClient('localhost', 27017)
+db = client.test_database
+posts = db.posts
+import datetime
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+post_id = posts.insert_one(post).inserted_id
+db.collection_names(include_system_collections=False)
+print posts.find_one()
 
