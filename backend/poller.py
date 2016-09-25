7162aa9aeafa149b26593bd69fd8339851c3d3e6
Jeremy Lerner <jeremy@SINT>
2016-09-24 19:55:52 -0700

    Implemented notifications of new requests

--- backend/poller.py
+++ backend/poller.py
@@ -1,6 +1,16 @@
 import gevent
+import logic
+import json
 
 def poller_function(username, my_queue):
 	while True:
-		gevent.sleep(1)
-        my_queue.put('data: {"jsonrpc": "2.0","method": "hey_test", "params": {"user_email": "'+username+'"}}\n\n')
+        #my_queue.put('data: {"jsonrpc": "2.0","method": "hey_test", "params": {"user_email": "'+username+'"}}\n\n')
+		gevent.sleep(0.5)
+		requests = logic.update_status(username)
+
+		for request in requests:
+			my_queue.put("data: " + json.dumps({
+					"jsonrpc": "2.0",
+					"method": "hey_request",
+					"params": request
+				}) + "\n\n")
